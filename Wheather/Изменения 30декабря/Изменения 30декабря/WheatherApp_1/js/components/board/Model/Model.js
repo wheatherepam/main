935495a298e70cca586e3a9305b9f0e4add596da
Unknow <this@ukr.net>
2015-01-05 03:21:54 +0400

    New changes

--- Wheather/Изменения 30декабря/Изменения 30декабря/WheatherApp_1/js/components/board/Model/Model.js
+++ Wheather/Изменения 30декабря/Изменения 30декабря/WheatherApp_1/js/components/board/Model/Model.js
@@ -1,6 +1,8 @@
 define(['Vendor','./Forecast'],function(Vendor,Forecast){
     'use strict'
-
+/**
+ * get data from Forecast and make model using in View
+**/
     var _=Vendor._,
         Class=Vendor.util.Class,
         Forecast=Forecast;
@@ -9,17 +11,28 @@ define(['Vendor','./Forecast'],function(Vendor,Forecast){
     Model= Class.extend({
             defaultOptions:{},
 
-            constructor:function(Forecast){
-                this.options= _.extend({},this.defaultOptions,Forecast);
+            constructor:function(id,city){
+                this.options= _.extend({},this.defaultOptions);
                 this.initialize();
             },
 
             initialize:function(){
-                var self=this;
-            }
-        }
+                var filterData=null;
 
-    );
+                this.render(id,city);
+            },
+
+            render:function(id,city){
+
+                var self=this,
+                    Forecast=new Forecast(id,city),
+                    data=Forecast.forecast;
+
+                _(data).forEach(function(key){
+                    self.filterData
+                })
+            }
+    });
 
     return Model;
 
